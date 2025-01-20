076b51a2d8d7ed11fbea5cc180f1e8513c520c52
masksjakqkess <2089142678@qq.com>
2025-01-20 11:13:54 +0800

    Add files via upload

---
+++ 神源码.lua
@@ -0,0 +1,9 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "成功执行神脚本",
+    Text = "你好啊 人机 玩个萝卜肉丝还要桂",
+    Icon = "rbxassetid://12978732658",
+    Duration = 60,
+    Callback = bindable,
+    Button1 = "感谢使用",
+    Button2 = "曼波"
+})
